82898f6d3fa51e1aed425dc6a20290de365f88e7
Kevin Machado Gamboa <ing.kevin@hotmail.com>
2020-04-24 10:18:18 +0100

    Adding Notebook for data loading & preprocess

--- Participants/Kevin/pre_process_ex1.py
+++ Participants/Kevin/pre_process_ex1.py
@@ -6,25 +6,30 @@ AI Diagnostics Hackathon - Fundamental Respiration Sound
 
 Ref:
     Kaggle:
-        
+        https://www.kaggle.com/vbookshelf/respiratory-sound-database
     Librosa demo:
         https://nbviewer.jupyter.org/github/librosa/librosa/blob/master/examples/LibROSA%20demo.ipynb
 """
-import aidiagnostics_toolbox as aidt
+
 
 import numpy as np
 import librosa as lb
 from tqdm import tqdm
 import soundfile as sfile
-# And the display module for visualization
-import librosa.display
+import librosa.display as lbdis
 import matplotlib.pyplot as plt
 #%matplotlib inline
-from scipy.io import wavfile as wf
+
 
 # getting file path
+import os
+import sys
 from os.path import dirname as up
-mf_path = up(up(up(__file__))).replace('\\','/')    # Main Folder Path
+mf_path = up(up(os.getcwd())).replace('\\','/')    # Main Folder Path
+sys.path.insert(1, mf_path)
+
+import aidiagnostics_toolbox as aidt
+
 # changing to database path
 import os
 import glob
@@ -40,10 +45,7 @@ sf_reco = []
 for file in tqdm(glob.glob("*.wav")):
     sound_list.append(file)
     # Loading and resampling sounds to 2kHz with librosa is too slow
-    # data, sf = lb.load(mf_path + '/Respiratory_Sound_Database/audio_and_txt_files/' + file, sr = 2000)
-    data, sf = sfile.read(mf_path + '/Respiratory_Sound_Database/audio_and_txt_files/' + file)
-    # Signal Basic Preprocess
- #   data, sf = aidt.pre_pro_audio_PCG(data, sf)
+    data, sf = sfile.read(mf_path + '/Respiratory_Sound_Database/audio_and_txt_files/' + file)#, sr = 2000)
     sound_reco.append(data)
     sf_reco.append(sf)
     
@@ -58,17 +60,15 @@ X = aidt.butter_bp_fil(X, 100, 300, sf)
 # -----------------------------------------------------------------------------
 #                          Mel Spectrogram
 # -----------------------------------------------------------------------------
-# Let's make and display a mel-scaled power (energy-squared) spectrogram
-S = lb.feature.melspectrogram(X, sr=sf, n_mels=128)
-# Convert to log scale (dB). We'll use the peak power (max) as reference.
-log_S = lb.power_to_db(S, ref=np.max)
-# Make a new figure
+# Mel spectrogram
+Mspec = lb.feature.melspectrogram(X, sr=sf, n_mels=128)
+# Convert to log scale (dB)
+Mspec_db = lb.power_to_db(Mspec, ref=np.max)
+# Plotting Mel Spectrogram
 plt.figure(figsize=(12,4))
 plt.subplot(2,1,1)
-# Display the spectrogram on a mel scale
-# sample rate and hop length parameters are used to render the time axis
-lb.display.specshow(log_S, sr=sf, x_axis='time', y_axis='mel')
-# Put a descriptive title on the plot
+
+lbdis.specshow(Mspec_db, sr=sf, x_axis='time', y_axis='mel')
 plt.title('mel power spectrogram')
 # draw a color bar
 #plt.colorbar(format='%+02.0f dB')
@@ -84,24 +84,24 @@ plt.tight_layout()
 y_harmonic, y_percussive = lb.effects.hpss(X)
 # What do the spectrograms look like?
 # Let's make and display a mel-scaled power (energy-squared) spectrogram
-S_harmonic   = librosa.feature.melspectrogram(y_harmonic)#, sr=sf)
-S_percussive = librosa.feature.melspectrogram(y_percussive)#, sr=sf)
+S_harmonic   = lb.feature.melspectrogram(y_harmonic)#, sr=sf)
+S_percussive = lb.feature.melspectrogram(y_percussive)#, sr=sf)
 
 # Convert to log scale (dB). We'll use the peak power as reference.
-log_Sh = librosa.power_to_db(S_harmonic, ref=np.max)
-log_Sp = librosa.power_to_db(S_percussive, ref=np.max)
+log_Sh = lb.power_to_db(S_harmonic, ref=np.max)
+log_Sp = lb.power_to_db(S_percussive, ref=np.max)
 
 # Make a new figure
 plt.figure(figsize=(12,6))
 plt.subplot(2,1,1)
 # Display the spectrogram on a mel scale
-librosa.display.specshow(log_Sh, sr=sf, y_axis='mel')
+lbdis.specshow(log_Sh, sr=sf, y_axis='mel')
 # Put a descriptive title on the plot
 plt.title('mel power spectrogram (Harmonic)')
 # draw a color bar
 #plt.colorbar(format='%+02.0f dB')
 plt.subplot(2,1,2)
-librosa.display.specshow(log_Sp, sr=sf, x_axis='time', y_axis='mel')
+lbdis.specshow(log_Sp, sr=sf, x_axis='time', y_axis='mel')
 # Put a descriptive title on the plot
 plt.title('mel power spectrogram (Percussive)')
 # draw a color bar
@@ -124,13 +124,12 @@ for i in range(k):
     
     # We'll use a CQT-based chromagram with 36 bins-per-octave in the CQT analysis.  An STFT-based implementation also exists in chroma_stft()
     # We'll use the harmonic component to avoid pollution from transients
-    C = librosa.feature.chroma_cqt(y=y_harmonic, sr=sf, bins_per_octave=int(octa_vec[i]))
+    C = lb.feature.chroma_cqt(y=y_harmonic, sr=sf, bins_per_octave=int(octa_vec[i]))
     # Display the chromagram: the energy in each chromatic pitch class as a function of time
     # To make sure that the colors span the full range of chroma values, set vmin and vmax
-    librosa.display.specshow(C, sr=sf, x_axis='time', y_axis='chroma', vmin=0, vmax=1)
-    
+    lbdis.specshow(C, sr=sf, x_axis='time', y_axis='chroma', vmin=0, vmax=1)   
     plt.title('Chromagram octaves %s' % str(octa_vec[i]))
-#plt.colorbar()
+
 plt.tight_layout()
 #%%
 # -----------------------------------------------------------------------------
@@ -139,14 +138,14 @@ plt.tight_layout()
 # Now, let's run the beat tracker.
 # We'll use the percussive component for this part
 plt.figure(figsize=(12, 6))
-tempo, beats = librosa.beat.beat_track(y=y_percussive, sr=sf)
+tempo, beats = lb.beat.beat_track(y=y_percussive, sr=sf)
 
 # Let's re-draw the spectrogram, but this time, overlay the detected beats
 plt.figure(figsize=(12,4))
-librosa.display.specshow(log_S, sr=sf, x_axis='time', y_axis='mel')
+lbdis.specshow(log_S, sr=sf, x_axis='time', y_axis='mel')
 
 # Let's draw transparent lines over the beat frames
-plt.vlines(librosa.frames_to_time(beats),
+plt.vlines(lb.frames_to_time(beats),
             1, 0.5 * sf,
             colors='w', linestyles='-', linewidth=2, alpha=0.5)
 
